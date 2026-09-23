3c47121aaef64546f2c11e8be207d4600ea3b4d0
jpaulorbbr <joaopbrbr@gmail.com>
2026-09-23 21:21:26 +0000

    2026-09-23-2

--- ecommerce/frontend/vite.config.js
+++ ecommerce/frontend/vite.config.js
@@ -1,32 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8000',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  },
-
   build: {
     outDir: '../static/dist',
     emptyOutDir: true,
     rollupOptions: {
-      input: {
-        main: './src/main.jsx',
-        admin: './src/admin/main.jsx'   // ← Entry point do admin
-      },
       output: {
-        entryFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]' // Garante que o CSS vire assets/main.css e assets/admin.css
       }
     }
-  },
-
-  base: '/static/dist/',
-})
+  }
+});
